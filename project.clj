b7eb80fc8c8d3c2d2740c7bf6c5303768466bdf6
Juho Teperi <juho.teperi@iki.fi>
2015-12-23 17:03:06 +0200

    Bump version to 0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deraen/less4clj "0.3.4-SNAPSHOT"
+(defproject deraen/less4clj "0.4.0-SNAPSHOT"
   :description "Wrapper for Less4j"
   :url "https://github.com/deraen/less4clj"
   :license {:name "Eclipse Public License"
