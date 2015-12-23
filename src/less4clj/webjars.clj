b8b4b325248f9b7c7a00218a994506f69575c524
Juho Teperi <juho.teperi@iki.fi>
2015-12-23 16:37:07 +0200

    Use webjars-locator instead of local clj implementation

--- src/less4clj/webjars.clj
+++ src/less4clj/webjars.clj
@@ -1,29 +1,6 @@
 (ns less4clj.webjars
-  (:require [clojure.string :as string]
-            [clojure.java.io :as io]
-            [less4clj.util :as util])
-  (:import [java.io IOException File]
-           [java.lang ClassLoader]
-           [java.util ServiceLoader]
-           [java.util.jar JarFile JarEntry JarInputStream]))
-
-(def WEBJARS_PATH_PREFIX "META-INF/resources/webjars")
-
-(defn list-assets [classloader]
-  (->> (.getResources classloader WEBJARS_PATH_PREFIX)
-       enumeration-seq
-       (mapcat
-         (fn [url]
-           (case (.getProtocol url)
-             "jar"
-             (let [[_ jar] (re-find #"^file:(.*\.jar)\!/.*$" (.getPath url))]
-               (->> (enumeration-seq (.entries (JarFile. (io/file jar))))
-                    (remove #(.isDirectory %))
-                    (map #(.getName %))
-                    (filter #(.startsWith % WEBJARS_PATH_PREFIX))))
-
-             (util/dbug "Skipping url: %s\n" url))))
-       set))
+  (:import [java.io File]
+           [org.webjars WebJarAssetLocator]))
 
 (def ^:private webjars-pattern
   #"META-INF/resources/webjars/([^/]+)/([^/]+)/(.*)")
@@ -32,8 +9,11 @@
   (let [[_ name version path] (re-matches webjars-pattern resource)]
     (str name File/separator path)))
 
-(defn asset-map []
-  (->> (list-assets (.getContextClassLoader (Thread/currentThread)))
+(defn asset-map
+  "Create map of asset path to classpath resource url. Asset path is
+  the resource url without webjars part."
+  []
+  (->> (.listAssets (WebJarAssetLocator.) "")
        (map (juxt asset-path identity))
        (into {})))
 
