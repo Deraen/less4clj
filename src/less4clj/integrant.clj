558c10aa92feaaee0145a9fdb6ddb9fc2752cecf
Juho Teperi <juho.teperi@iki.fi>
2019-08-28 09:51:44 +0300

    Fix integrant resume-key
    https://github.com/Deraen/sass4clj/pull/30/

--- src/less4clj/integrant.clj
+++ src/less4clj/integrant.clj
@@ -5,15 +5,15 @@
 (defmethod ig/init-key ::less4clj [_ options]
   (api/start options))
 
-(defmethod ig/halt-key! ::less4clj [this options]
-  (api/stop this))
+(defmethod ig/halt-key! ::less4clj [_ watcher]
+  (api/stop watcher))
 
-(defmethod ig/suspend-key! ::less4clj [this options]
+(defmethod ig/suspend-key! ::less4clj [_ watcher]
   nil)
 
 (defmethod ig/resume-key ::less4clj [key opts old-opts old-impl]
   (if (= opts old-opts)
     old-impl
     (do
-      (ig/halt-key! key old-opts)
+      (ig/halt-key! key old-impl)
       (ig/init-key key opts))))
